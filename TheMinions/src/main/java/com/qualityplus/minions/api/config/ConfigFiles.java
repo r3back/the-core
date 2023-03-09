99ca4bf9c6aeced74c72b51f846cdc213d6cc2dd
Zanganito <zanganokill@gmail.com>
2023-03-09 20:18:34 -0300

    Inject files of reload

--- TheMinions/src/main/java/com/qualityplus/minions/api/config/ConfigFiles.java
+++ TheMinions/src/main/java/com/qualityplus/minions/api/config/ConfigFiles.java
@@ -1,8 +1,20 @@
 package com.qualityplus.minions.api.config;
 
 import com.qualityplus.assistant.api.config.ConfigReloader;
+import com.qualityplus.minions.base.config.minions.CowMinion;
+import com.qualityplus.minions.base.config.minions.DiamondMinion;
+import com.qualityplus.minions.base.config.minions.SnowMinion;
+import com.qualityplus.minions.base.minions.entity.type.CropBreakMinion;
 
 public interface ConfigFiles<C, I, M, CMD, S, AS, FU, U> extends ConfigReloader {
+    CowMinion cowMinion();
+
+    SnowMinion snowMinion();
+
+    CropBreakMinion cropBreakMinion();
+
+    DiamondMinion diamondMinion();
+
     C config();
     I inventories();
     M messages();
